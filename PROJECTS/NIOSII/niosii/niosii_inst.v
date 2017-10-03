0c57234a73c6973ae797357f3e2dbdeb35cecb45
boffkolt <boffkolt@gmail.com>
2017-10-03 11:17:27 +0300

    vv

---
+++ PROJECTS/NIOSII/niosii/niosii_inst.v
@@ -0,0 +1,6 @@
+	niosii u0 (
+		.buts_external_connection_export (<connected-to-buts_external_connection_export>), // buts_external_connection.export
+		.clk_clk                         (<connected-to-clk_clk>),                         //                      clk.clk
+		.leds_external_connection_export (<connected-to-leds_external_connection_export>)  // leds_external_connection.export
+	);
+
