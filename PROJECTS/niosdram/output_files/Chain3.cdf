cfecd022ba2183b1b9008ab7964afbbd9ec10d4d
boffkolt <v.kolot@elsiel.ru>
2017-10-23 17:11:10 +0300

    re

---
+++ PROJECTS/niosdram/output_files/Chain3.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 16.1.2 Build 203 01/18/2017 SJ Standard Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE6E22) Path("/home/engineer/GIT/FPGA/PROJECTS/niosdram/output_files/") File("niosdram.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ PROJECTS/niosdram/unsaved/unsaved_inst.v
@@ -0,0 +1,14 @@
+	unsaved u0 (
+		.clk_clk                           (<connected-to-clk_clk>),                           //                         clk.clk
+		.new_sdram_controller_0_wire_addr  (<connected-to-new_sdram_controller_0_wire_addr>),  // new_sdram_controller_0_wire.addr
+		.new_sdram_controller_0_wire_ba    (<connected-to-new_sdram_controller_0_wire_ba>),    //                            .ba
+		.new_sdram_controller_0_wire_cas_n (<connected-to-new_sdram_controller_0_wire_cas_n>), //                            .cas_n
+		.new_sdram_controller_0_wire_cke   (<connected-to-new_sdram_controller_0_wire_cke>),   //                            .cke
+		.new_sdram_controller_0_wire_cs_n  (<connected-to-new_sdram_controller_0_wire_cs_n>),  //                            .cs_n
+		.new_sdram_controller_0_wire_dq    (<connected-to-new_sdram_controller_0_wire_dq>),    //                            .dq
+		.new_sdram_controller_0_wire_dqm   (<connected-to-new_sdram_controller_0_wire_dqm>),   //                            .dqm
+		.new_sdram_controller_0_wire_ras_n (<connected-to-new_sdram_controller_0_wire_ras_n>), //                            .ras_n
+		.new_sdram_controller_0_wire_we_n  (<connected-to-new_sdram_controller_0_wire_we_n>),  //                            .we_n
+		.pio_0_external_connection_export  (<connected-to-pio_0_external_connection_export>)   //   pio_0_external_connection.export
+	);
+
